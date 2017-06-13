76e6fca1d8ae36ab37bd2a786a2adaa07100f5a9
shenqin <shenqin@yunbeitech.com>
2017-06-13 14:47:45 +0800

    merge test

---
+++ src/main/java/com/example/packge.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Author:shenqin
+ * version: V1.0
+ * Date: 2017/6/13
+ * Time: 14:33
+ * Description:
+ */
+public class packge {
+
+    public static void main(String[] args) {
+        System.out.println("ss");
+    }
+
+}
